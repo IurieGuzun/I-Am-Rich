c3c7ca1085772946ae1383e0d7b4ffbf0f50a036
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-09 12:15:16 -0400

    Configure Label & Image

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -10,10 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I am Rich!")
+                    .font(.custom("Times New Roman", size: 30))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.red)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
